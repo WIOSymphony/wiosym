64bfbef222a19bc56c2bee78c7a5af521aa7d068
EdSacre <88026687+EdSacre@users.noreply.github.com>
2024-04-12 10:16:54 +0200

    Update sitemap

--- metadata/sitemap_create.R
+++ metadata/sitemap_create.R
@@ -5,7 +5,7 @@ dir <- paste0("../products/v", ver, "/output_geotiffs")
 l <- list.files(path = dir, pattern = ".json", recursive = T, full.names = T)
 l.short <- list.files(path = dir, pattern = ".json", recursive = T, full.names = F)
 
-root <- xml_new_root("urlset", ns = "https://www.sitemaps.org/schemas/sitemap/0.9/")
+root <- xml_new_root("urlset", xmlns = "https://www.sitemaps.org/schemas/sitemap/0.9/")
 
 for(i in 1:length(l)){
   turl <- paste0("https://raw.githubusercontent.com/WIOSymphony/wiosym/main/products/v2.1/output_geotiffs/", l.short[i])
