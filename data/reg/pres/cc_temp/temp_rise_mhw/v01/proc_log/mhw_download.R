e0c327ee96cb3c9729874ce33c5fb84707b0d7f0
EdSacre <88026687+EdSacre@users.noreply.github.com>
2024-05-21 08:26:40 +0200

    Update layers and add raw data

--- data/reg/pres/cc_temp/temp_rise_mhw/v01/proc_log/mhw_download.R
+++ data/reg/pres/cc_temp/temp_rise_mhw/v01/proc_log/mhw_download.R
@@ -19,7 +19,7 @@ script_path <- "./process/r"
 (dest1_path_file1 <-paste0(dest1_path, "/", subtheme1, "_", name1, "_", version, d_version, ".tif"))
 
 # Repeat for each year - data available from 1985
-for(i in 2000:2020){
+for(i in 1994:1999){
 
   yr <- i
   dir.create(paste0(work1_path, "/mhw_noaa/", yr))
